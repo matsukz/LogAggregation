69aca23b722e30f70333208d288e64981f323382
まつ <92625267+matsukz@users.noreply.github.com>
2024-01-29 13:17:19 +0000

    DBに最低限接続できる

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # LogAggregation
 NGINXのアクセスログを集計してWebhookへポスト
 
+# 使い方
+Nginxのアクセスログを`fluent/fluentd:v1.16-1`を用いてMySQLに登録するシステムを作っておきます。
+
+そのMySQLサーバーに向けてsqlalchemyを接続しましょう。
+
 # 変更歴
 ## 2024-01-29
 * 初期作成
--- python/app.py
+++ python/app.py
@@ -1,9 +1,65 @@
+# discord関連
 from discordwebhook import Discord
-import sqlalchemy
-from sqlalchemy.sql import text
-import ping3
-import requests
+
+# DB関連
+from sqlalchemy import Column
+from sqlalchemy import Integer,BigInteger,String,DateTime
+from sqlalchemy import func
+
+from sqlalchemy.future import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+#環境変数関連
 import os
 from dotenv import load_dotenv
+
+#その他
+import ping3
+import requests
+import datetime
+
+#環境変数読み込み
+load_dotenv()
+db_name = os.environ["db_name"]
+db_user = os.environ["db_user"]
+db_password = os.environ["db_passwd"]
+db_address = os.environ["db_address"]
+#-------
+
+#DB接続
+engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_address}/{db_name}?charset=utf8")
+Base = declarative_base()
+#-------
+
+#テーブル情報
+class NnginxLog(Base):
+    __tablename__ = "nginx_access_log"
+    id = Column("id",BigInteger,primary_key=True)
+    time = Column("time",DateTime)
+    remote_addr = Column("remote_addr",String)
+    country = Column("country",String)
+    status = Column("Status",Integer)
+#------
+
+#セッション作成
+Session = sessionmaker(bind=engine)
+session = Session()
+#------
+
+#SQL実行
+last = session.query(NnginxLog).limit(3).all()
+#------
+
+#データ取り出し
+for i in last:
+    print(f"id={i.id} IP={i.remote_addr}")
+#------
+
+#セッション終了
+session.close()
+#------
+
 
 print("OK")
